ab4aa7606804caf392a95249fa4a53ce32746c20
lenicliu <lenicliu@163.com>
2015-11-30 22:04:59 +0800

    reactor

--- tutorials-framework/tutorials-spring/tutorials-spring-boot/jboot-hateoas-application/src/main/java/com/lenicliu/jboot/hateoas/PostController.java
+++ tutorials-framework/tutorials-spring/tutorials-spring-boot/jboot-hateoas-application/src/main/java/com/lenicliu/jboot/hateoas/PostController.java
@@ -5,13 +5,9 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
@@ -24,40 +20,67 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * GET		/posts
+ * 
+ * POST		/posts
+ * GET		/posts/{id}
+ * PUT		/posts/{id}
+ * DELETE	/posts/{id}
+ * 
+ * Simple CRUD Hypemedia-Driven RESTfull Service
+ * 
+ * @author lenicliu
+ *
+ */
 @RestController
 @RequestMapping("/posts")
-@ExposesResourceFor(Post.class)
 public class PostController {
 
-	private static AtomicLong		ID		= new AtomicLong(1);
-	private static Map<Long, Post>	POSTS	= new HashMap<Long, Post>();
+	@Autowired
+	private PostService postService;
 
-	static {
-		Post post1 = new Post();
-		post1.setAuthor("lenicliu");
-		post1.setCreated(new Date());
-		post1.setId(ID.getAndIncrement());
-		post1.setState(Post.State.PUBLISH);
-		post1.setTitle("Hi, Hateoas");
-		POSTS.put(post1.getId(), post1);
-
-		Post post2 = new Post();
-		post2.setAuthor("kitty");
-		post2.setCreated(new Date());
-		post2.setId(ID.getAndIncrement());
-		post2.setState(Post.State.DRAFT);
-		post2.setTitle("Spring-Boot-Hateoas");
-		POSTS.put(post2.getId(), post2);
-		
-		Post post3 = new Post();
-		post3.setAuthor("lenic & kitty");
-		post3.setCreated(new Date());
-		post3.setId(ID.getAndIncrement());
-		post3.setState(Post.State.PUBLISH);
-		post3.setTitle("I don't care");
-		POSTS.put(post3.getId(), post3);
+	@RequestMapping(method = RequestMethod.GET)
+	public HttpEntity<Resources<Resource<Post>>> get() {
+		List<Post> posts = postService.findAll();
+		if (posts == null || posts.isEmpty()) {
+			return blank();
+		}
+		Link _self = linkTo(methodOn(getClass()).get()).withSelfRel();
+		return ok(new Resources<Resource<Post>>(linkPosts(posts), _self));
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public HttpEntity<Resource<Post>> get(@PathVariable Long id) {
+		Post post = postService.find(id);
+		if (post == null || post.getId() == null) {
+			return notFound();
+		}
+		return ok(linkPost(post));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public HttpEntity<Resource<Post>> post(@RequestBody Post post) {
+		postService.create(post);
+		return ok();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public HttpEntity<Resource<Post>> put(@PathVariable Long id, @RequestBody Post post) {
+		postService.modify(id, post);
+		return ok();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public HttpEntity<Resource<Post>> delete(@PathVariable Long id) {
+		Post post = postService.find(id);
+		if (post == null || post.getId() == null) {
+			return notFound();
+		}
+		postService.delete(id);
+		return ok();
+	}
+	
 	private <T> HttpEntity<T> ok(T value) {
 		return new ResponseEntity<T>(value, HttpStatus.OK);
 	}
@@ -72,55 +95,6 @@ public class PostController {
 
 	private <T> HttpEntity<T> notFound() {
 		return new ResponseEntity<T>(HttpStatus.NOT_FOUND);
-	}
-
-	@RequestMapping(method = RequestMethod.GET)
-	public HttpEntity<Resources<Resource<Post>>> get() {
-		List<Post> posts = new ArrayList<Post>(POSTS.values());
-		if (posts == null || posts.isEmpty()) {
-			return blank();
-		}
-		Link _self = linkTo(methodOn(getClass()).get()).withSelfRel();
-		return ok(new Resources<Resource<Post>>(linkPosts(posts), _self));
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public HttpEntity<Resource<Post>> get(@PathVariable Long id) {
-		Post post = POSTS.get(id);
-		if (post == null || post.getId() == null) {
-			return notFound();
-		}
-		return ok(linkPost(post));
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public HttpEntity<Resource<Post>> post(@RequestBody Post post) {
-		post.setId(ID.getAndIncrement());
-		post.setCreated(new Date());
-		POSTS.put(post.getId(), post);
-		return ok(linkPost(post));
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public HttpEntity<Resource<Post>> put(@PathVariable Long id, @RequestBody Post post) {
-		Post exist = POSTS.remove(id);
-		if (exist == null) {
-			return notFound();
-		}
-		exist.setAuthor(post.getAuthor());
-		exist.setState(post.getState());
-		exist.setTitle(post.getTitle());
-		POSTS.put(exist.getId(), exist);
-		return ok(linkPost(exist));
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public HttpEntity<Resource<Post>> delete(@PathVariable Long id) {
-		Post removed = POSTS.remove(id);
-		if (removed == null || removed.getId() == null) {
-			return notFound();
-		}
-		return ok();
 	}
 
 	private Resource<Post> linkPost(Post post) {
