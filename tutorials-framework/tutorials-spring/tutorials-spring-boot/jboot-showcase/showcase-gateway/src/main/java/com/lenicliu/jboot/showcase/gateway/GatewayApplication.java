3fd2c34ba96a5aaf0ca39b8d173431787efddff7
lenicliu <lenicliu@163.com>
2015-12-30 16:39:58 +0800

    spring-boot-showcase

---
+++ tutorials-framework/tutorials-spring/tutorials-spring-boot/jboot-showcase/showcase-gateway/src/main/java/com/lenicliu/jboot/showcase/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.lenicliu.jboot.showcase.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class GatewayApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
