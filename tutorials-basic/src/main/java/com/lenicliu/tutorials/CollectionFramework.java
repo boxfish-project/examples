3faf39121d08b535b8c47bd3ae8bef9fd017bb3c
lenicliu <lenicliu@163.com>
2016-02-06 01:39:56 +0800

    remove all

--- tutorials-basic/src/main/java/com/lenicliu/tutorials/CollectionFramework.java
+++
@@ -1,8 +0,0 @@
-package com.lenicliu.tutorials;
-
-public class CollectionFramework {
-
-	public static void main(String[] args) {
-		
-	}
-}
--- tutorials-framework/tutorials-spring/tutorials-spring-boot/jboot-rmi-application/src/main/java/com/lenicliu/jboot/rmi/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.lenicliu.jboot.rmi;
-
-public interface UserService {
-
-	String find(Long id);
-}
